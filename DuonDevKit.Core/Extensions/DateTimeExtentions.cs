8e653d865cd74a0045dbb65c1d6efd043320b676
DuongDT <duongdangtruong.it@gmail.com>
2026-08-09 21:17:29 +0700

    Add DateTimeExtentions

---
+++ DuonDevKit.Core/Extensions/DateTimeExtentions.cs
@@ -0,0 +1,37 @@
+﻿namespace DuonDevKit.Core.Extensions
+{
+    public static class DateTimeExtentions
+    {
+        public static DateTime StartOfWeek(this DateTime dateTime, DayOfWeek startOfWeek = DayOfWeek.Monday)
+        {
+            int diff = (7 + (dateTime.DayOfWeek - startOfWeek)) % 7;
+            return dateTime.AddDays(-1 * diff).Date;
+        }
+
+        public static DateTime EndOfWeek(this DateTime dateTime, DayOfWeek startOfWeek = DayOfWeek.Monday)
+        {
+            return dateTime.StartOfWeek(startOfWeek).AddDays(6);
+        }
+
+        public static DateTime StartOfMonth(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, 1);
+        }
+
+        public static DateTime EndOfMonth(this DateTime dateTime)
+        {
+            return dateTime.StartOfMonth().AddMonths(1).AddDays(-1);
+        }
+
+        public static DateTime StartOfYear(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, 1, 1);
+        }
+
+        public static DateTime EndOfYear(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, 12, 31);
+        }
+
+    }
+}
